33405db19e8accbbf88d100d49e491df503ed681
Pavlo Izhytskyi <kn1b21.izhytskyi@kpnu.edu.ua>
2024-10-15 14:27:59 +0300

    added pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common'; // Потрібно для використання пайпів, таких як 'number'
 
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    CommonModule // Імпорт CommonModule для доступу до пайпів
+    CommonModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }) // Імпорт CommonModule для доступу до пайпів
   ],
   providers: [],
   bootstrap: [AppComponent]
