1f37d273bc4d30bf41045604fe6dacd545fcc797
Pavlo Izhytskyi <96439436+izhytskyi@users.noreply.github.com>
2024-10-15 13:41:09 +0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { CommonModule } from '@angular/common'; // Додано для доступу до пайпів, таких як 'number'
+import { CommonModule } from '@angular/common'; // Потрібно для використання пайпів, таких як 'number'
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent // Реєстрація компонента
+    AppComponent // Реєстрація вашого компонента
   ],
   imports: [
     BrowserModule,
-    CommonModule // Імпорт CommonModule для пайпів
+    CommonModule // Імпорт CommonModule для доступу до пайпів
   ],
   providers: [],
   bootstrap: [AppComponent]
