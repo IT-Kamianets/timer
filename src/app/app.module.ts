416ab08736012daaede42b9d494f987f38b22497
Yura Stefura <99644084+Stefura@users.noreply.github.com>
2024-10-22 13:48:41 +0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,4 @@
-import { NgModule, isDevMode } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { CommonModule } from '@angular/common'; // Потрібно для використання пайпів, таких як 'number'
-
-import { AppComponent } from './app.component';
 import { TimerComponent } from './timer/timer.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
 import { StopwatchComponent } from './stopwatch/stopwatch.component';
 
 @NgModule({
@@ -18,12 +12,11 @@ import { StopwatchComponent } from './stopwatch/stopwatch.component';
     CommonModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: !isDevMode(),
-      // Register the ServiceWorker as soon as the application is stable
-      // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000'
-    }) // Імпорт CommonModule для доступу до пайпів
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
