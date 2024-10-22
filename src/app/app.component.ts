b82b9270acc1502808991b2afd80f21f5a0480af
Pavlo Izhytskyi <kn1b21.izhytskyi@kpnu.edu.ua>
2024-10-22 12:33:22 +0300

    add stopwatch component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,34 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'] // Додай стиль, якщо потрібно
 })
 export class AppComponent {
-  minutes: number = 0;
-  seconds: number = 0;
-  private timerInterval: any;
-  isRunning: boolean = false; // Додай цю змінну
-
-  startTimer() {
-    if (!this.isRunning) { // Перевірка, чи таймер не запущений
-      this.isRunning = true; // Встановлюємо, що таймер запущений
-      this.timerInterval = setInterval(() => {
-        if (this.seconds === 59) {
-          this.minutes++;
-          this.seconds = 0;
-        } else {
-          this.seconds++;
-        }
-      }, 1000);
-    }
-  }
-
-  pauseTimer() {
-    this.isRunning = false; // Встановлюємо, що таймер зупинений
-    clearInterval(this.timerInterval);
-  }
-
-  resetTimer() {
-    this.isRunning = false; // Встановлюємо, що таймер зупинений
-    clearInterval(this.timerInterval);
-    this.minutes = 0;
-    this.seconds = 0;
-  }
+  
 }
---
+++ src/app/timer/timer.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-timer',
+  standalone: true,
+  imports: [],
+  templateUrl: './timer.component.html',
+  styleUrl: './timer.component.css'
+})
+export class TimerComponent {
+  minutes: number = 0;
+  seconds: number = 0;
+  private timerInterval: any;
+  isRunning: boolean = false; // Додай цю змінну
+
+  startTimer() {
+    if (!this.isRunning) { // Перевірка, чи таймер не запущений
+      this.isRunning = true; // Встановлюємо, що таймер запущений
+      this.timerInterval = setInterval(() => {
+        if (this.seconds === 59) {
+          this.minutes++;
+          this.seconds = 0;
+        } else {
+          this.seconds++;
+        }
+      }, 1000);
+    }
+  }
+
+  pauseTimer() {
+    this.isRunning = false; // Встановлюємо, що таймер зупинений
+    clearInterval(this.timerInterval);
+  }
+
+  resetTimer() {
+    this.isRunning = false; // Встановлюємо, що таймер зупинений
+    clearInterval(this.timerInterval);
+    this.minutes = 0;
+    this.seconds = 0;
+  }
+}
