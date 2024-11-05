90cb9511b30684fe40220dc477d0e79a92e30c66
Yura Stefura <99644084+Stefura@users.noreply.github.com>
2024-11-05 12:30:11 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
+// app.component.ts
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'] // Додай стиль, якщо потрібно
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'timer'; // Додайте цю рядок, щоб виправити помилку
-  type: 'timer' | 'stopwatch' = 'timer';
+  showTimer = true;
+
+  toggleView(showTimer: boolean) {
+    this.showTimer = showTimer;
+  }
 }
